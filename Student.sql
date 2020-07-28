8eb9aea89d1fe02c2a176f5b94f9062285d4fc9a
SaadatHussain586 <saadathussainofficial@gmail.com>
2020-07-28 18:39:32 +0500

    My very first commit

---
+++ Student.sql
@@ -0,0 +1,11 @@
+create database std;
+
+use std java;
+
+create table record
+(
+id int(5)  primary key auto_increment,
+name varchar(20) not null,
+number int(11) not null,
+course varchar(50) not null
+);
---
+++ src/java/util/logging.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java.util;
+
+/**
+ *
+ * @author Manto
+ */
+public class logging {
+
+    public static class Logger {
+
+        public static Object getLogger(String name) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        public Logger() {
+        }
+    }
+
+    public static class Level {
+
+        public Level() {
+        }
+    }
+    
+}
